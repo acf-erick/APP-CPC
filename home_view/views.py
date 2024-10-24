988b29cfb9a78787814027a987589bc57df554a5
Enzo de Oliveira Passos Correia <Enzo.OPC1@gmail.com>
2024-10-24 10:41:48 -0300

    Create PJ or PF template

--- home_view/views.py
+++ home_view/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'home_view/home.html'
+
+class PjPfView(TemplateView):
+    template_name = 'home_view/pjPf.html'
--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from home_view.views import HomeView
+from home_view.views import HomeView, PjPfView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
+    path('userType', PjPfView.as_view(), name='user_type')
 ]
