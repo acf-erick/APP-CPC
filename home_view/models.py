fc24a60bf76653a6924d6b281564c1019c65966f
Enzo de Oliveira Passos Correia <Enzo.OPC1@gmail.com>
2024-12-02 11:23:39 -0300

    Undo Models to only PF

--- home_view/models.py
+++ home_view/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from .models import PessoaFisica
+# from .models import PessoaFisica
 
 class PessoaFisica(models.Model):
     name = models.CharField(max_length=255, verbose_name="Nome Completo", null=False, blank=False)
@@ -9,70 +9,66 @@ class PessoaFisica(models.Model):
     cep = models.IntegerField(verbose_name="CEP", null=False, blank=False)
     phone = models.CharField(max_length=16, verbose_name="Celular", null=False, blank=False)
     email = models.EmailField(verbose_name="E-mail", max_length=255, null=False, blank=False)
-    password = models.CharField(verbose_name="Senha", null=False, blank=False)
+    password = models.CharField(max_length=255, verbose_name="Senha", null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
 
 
 
-class PessoaJuridica(PessoaFisica):  # Herda de PessoaFisica
-    company_name = models.CharField(max_length=255, verbose_name="Nome da Empresa", null=False, blank=False)
-    cnpj = models.CharField(max_length=14, verbose_name="CNPJ", null=False, blank=False)
-  
 
 #Início do CRUD da pessoa física
 #Criar pessoa física
 
-def create_pessoa_fisica(nome, rg, cpf, data_nascimento, celular, email):
-    pessoa = PessoaFisica.objects.create(
-        nome=nome,
-        rg=rg,
-        cpf=cpf,
-        data_nascimento=data_nascimento,
-        celular=celular,
-        email=email
-    )
-    print(f"Adicionado: {pessoa}")
+# def create_pessoa_fisica(nome, rg, cpf, data_nascimento, celular, email):
+#     pessoa = PessoaFisica.objects.create(
+#         nome=nome,
+#         rg=rg,
+#         cpf=cpf,
+#         data_nascimento=data_nascimento,
+#         celular=celular,
+#         email=email
+#     )
+#     print(f"Adicionado: {pessoa}")
 
 
 #Ler pessoas físicas
 
-def read_all_pessoas_fisicas():
-    pessoas = PessoaFisica.objects.all()
-    for pessoa in pessoas:
-        print(pessoa)
+# def read_all_pessoas_fisicas():
+#     pessoas = PessoaFisica.objects.all()
+#     for pessoa in pessoas:
+#         print(pessoa)
 
 #Atualizar pessoa física
 
-def update_pessoa_fisica(id, nome=None, rg=None, cpf=None, data_nascimento=None, celular=None, email=None):
-    if PessoaFisica.objects.filter(id=id).exists():
-        pessoa = PessoaFisica.objects.get(id=id)
+# def update_pessoa_fisica(id, nome=None, rg=None, cpf=None, data_nascimento=None, celular=None, email=None):
+#     if PessoaFisica.objects.filter(id=id).exists():
+#         pessoa = PessoaFisica.objects.get(id=id)
         
-        if nome is not None:
-            pessoa.name = nome
-        if rg is not None:
-            pessoa.rg = rg
-        if cpf is not None:
-            pessoa.cpf = cpf
-        if data_nascimento is not None:
-            pessoa.birth_date = data_nascimento
-        if celular is not None:
-            pessoa.phone = celular
-        if email is not None:
-            pessoa.email = email
+#         if nome is not None:
+#             pessoa.name = nome
+#         if rg is not None:
+#             pessoa.rg = rg
+#         if cpf is not None:
+#             pessoa.cpf = cpf
+#         if data_nascimento is not None:
+#             pessoa.birth_date = data_nascimento
+#         if celular is not None:
+#             pessoa.phone = celular
+#         if email is not None:
+#             pessoa.email = email
         
-        pessoa.save()
-        print(f"Atualizado: {pessoa}")
-    else:
-        print("Pessoa não encontrada.")
+#         pessoa.save()
+#         print(f"Atualizado: {pessoa}")
+#     else:
+#         print("Pessoa não encontrada.")
 
 #Deletar pessoa física
 
-def delete_pessoa_fisica(id):
-    if PessoaFisica.objects.filter(id=id).exists():
-        PessoaFisica.objects.filter(id=id).delete()
-        print(f"Pessoa com ID {id} foi excluída.")
-    else:
-        print("Pessoa não encontrada.")
+# def delete_pessoa_fisica(id):
+#     if PessoaFisica.objects.filter(id=id).exists():
+#         PessoaFisica.objects.filter(id=id).delete()
+#         print(f"Pessoa com ID {id} foi excluída.")
+#     else:
+#         print("Pessoa não encontrada.")
         
 #Na prática
 
