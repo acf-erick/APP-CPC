8bcae2ab8dcdd258837a07cac65a8634e71177a7
Rafaelpiazza11 <piazzalungarafael@gmail.com>
2024-12-03 12:17:00 -0300

    update lista parceiro

--- home_view/views.py
+++ home_view/views.py
@@ -16,6 +16,28 @@ class PjSignUpView(TemplateView):
 
 class FriedsListView(TemplateView):
     template_name = 'home_view/friendsList.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['parceiros'] = [
+            'Trilhando Caminhos Ecoturismo',
+            'Promoções e Eventos - Trilhas e Viagens',
+            'Mais Agai',
+            'Jardim dos Beija-Flores',
+            'Cervejas Capivara',
+            'Nascer do Sol',
+            'Queen of Peppers',
+            'Botica Caliandra',
+            'Queijaria Rancharia',
+            'Chácara Bosque do Cerrado',
+            'Território Comunicação',
+            'Suzana Cardoso',
+            'Ateliê Anjico',
+            'Ecs Fisioterapia',
+            'MEI',
+            'Brasília com Maria',
+            'Chácara BOAZ'
+        ]
+        return context
 
 class LoginView(TemplateView):
     template_name = 'home_view/login.html'
